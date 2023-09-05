1e55e1b86dfc0ac44ecd6f556a16b9b6723fc5ba
zouyuxiang <lal710609@gmail.com>
2023-09-05 13:40:30 +0800

    初始化 Spring Boot project

---
+++ springboot-mall/src/test/java/com/ivanzou/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanzou.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
