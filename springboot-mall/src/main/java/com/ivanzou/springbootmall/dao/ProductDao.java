503fb24d5c5ad84fbc578e2a96b6a87ae9b4d049
zouyuxiang <lal710609@gmail.com>
2023-09-08 13:51:32 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/ivanzou/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/ivanzou/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.ivanzou.springbootmall.dao;
 import com.ivanzou.springbootmall.dto.ProductRequest;
 import com.ivanzou.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- springboot-mall/src/main/java/com/ivanzou/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/ivanzou/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.ivanzou.springbootmall.service;
 import com.ivanzou.springbootmall.dto.ProductRequest;
 import com.ivanzou.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
