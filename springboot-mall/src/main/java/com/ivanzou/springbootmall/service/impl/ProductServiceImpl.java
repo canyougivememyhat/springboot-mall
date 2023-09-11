b2b7b393bec2c49f7140fd78b35e8271c48b3fad
zouyuxiang <lal710609@gmail.com>
2023-09-11 11:23:27 +0800

    商品功能 - 實作查詢商品列表 (查詢條件)

--- springboot-mall/src/main/java/com/ivanzou/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/ivanzou/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ivanzou.springbootmall.service.impl;
 
+import com.ivanzou.springbootmall.constant.ProductCategory;
 import com.ivanzou.springbootmall.dao.ProductDao;
 import com.ivanzou.springbootmall.dto.ProductRequest;
 import com.ivanzou.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
