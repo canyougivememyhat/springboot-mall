e26a51b412ed55e0dfae97a335754d0c6504a0b4
zouyuxiang <lal710609@gmail.com>
2023-09-12 15:15:22 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總筆數)

--- springboot-mall/src/main/java/com/ivanzou/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/ivanzou/springbootmall/dao/impl/ProductDaoImpl.java
@@ -21,6 +21,25 @@ public class ProductDaoImpl implements ProductDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
+    public Integer countProduct(ProductQueryParams productQueryParams) {
+        String sql = "SELECT count(*) FROM product WHERE 1=1";
+
+        Map<String, Object> map = new HashMap<>();
+
+        if(productQueryParams.getCategory() != null){
+            sql = sql + " AND category = :category";
+            map.put("category", productQueryParams.getCategory().name());
+        }
+
+        if(productQueryParams.getSearch() != null){
+            sql = sql + " AND product_name LIKE :search";
+            map.put("search", "%" +productQueryParams.getSearch()+ "%");
+        }
+
+        return namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+    }
+
+    @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
         String sql = "SELECT product_id, product_name, category, image_url, price, stock, " +
                 "description, created_date, last_modified_date FROM product WHERE 1=1";
@@ -45,9 +64,7 @@ public class ProductDaoImpl implements ProductDao {
         map.put("limit", productQueryParams.getLimit());
         map.put("offset", productQueryParams.getOffset());
 
-        List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
-
-        return productList;
+        return namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
     }
 
     @Override
